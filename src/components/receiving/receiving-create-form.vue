5d51f425ea3445d2a11be2caf38425d2c97b9346
Inglar <inglarg@gmail.com>
2019-07-18 14:08:41 +0500

    Внутренний функционал заявки на приём/выдачу контейнера.

--- src/components/receiving/receiving-create-form.vue
+++ src/components/receiving/receiving-create-form.vue
@@ -7,38 +7,38 @@
 
         <div class="application_wrap">
             <div class="application">
-                <h2>Заявка на прием/выдачу контейнера</h2>
+                <h2>Заявка на <span v-show="notSelectOut">прием</span><span v-show="notSelectIn && notSelectOut">/</span><span v-show="notSelectIn">выдачу</span> контейнера</h2>
                 <div class="form_row">
                     <div class="receiving_form_starting">
 
                         <div class="labels">
 
                             <label class="label_width_outside_input">
-                                <input type="radio" name="take_gave" v-model="WebGateIn" @click="setMainOperationIn(true)">
+                                <input type="radio" name="take_gave" value="WebGateIn" v-model="webGate">
                                 <span class="pseudo_checkbox"></span>
                                 <span class="title">Принять на терминал</span>
                             </label>
 
                             <label class="label_width_outside_input">
-                                <input type="radio" name="take_gave" v-model="WebGateOut" @click="setMainOperationIn(true)">
+                                <input type="radio" name="take_gave" value="WebGateOut" v-model="webGate">
                                 <span class="pseudo_checkbox"></span>
                                 <span class="title">Выдать с терминала</span>
                             </label>
 
                             <label class="label_width_outside_input">
-                                <input name="trucking_parameters_state" type="radio">
+                                <input name="trucking_parameters_state" :value="true" v-model="bidEmpty" type="radio" :disabled="notSelectedMainOperation">
                                 <span class="pseudo_checkbox"></span>
                                 <span class="title">Порожний</span>
                             </label>
 
                             <label class="label_width_outside_input">
-                                <input name="trucking_parameters_state" type="radio">
+                                <input name="trucking_parameters_state" :value="false" v-model="bidEmpty" type="radio" :disabled="notSelectedMainOperation">
                                 <span class="pseudo_checkbox"></span>
                                 <span class="title">Груженый</span>
                             </label>
 
                             <label class="label_width_outside_input">
-                                <input type="checkbox" v-model="DangerousGoods">
+                                <input type="checkbox" v-model="DangerousGoods" :disabled="notSelectedMainParams || bidEmpty !== false">
                                 <span class="pseudo_checkbox"></span>
                                 <span class="title">Опасный груз</span>
                             </label>
@@ -48,97 +48,41 @@
                         <div class="labels">
 
                             <label class="label_width_outside_input">
-                                <input type="checkbox" disabled>
+                                <input type="checkbox" v-model="WebCustomsRelease" :disabled="notSelectedMainParams">
                                 <span class="pseudo_checkbox"></span>
-                                <span class="title">заказать перемещение на/с СВХ</span>
+                                <span class="title">заказать перемещение <span v-show="notSelectOut">на</span><span v-show="notSelectIn && notSelectOut">/</span><span v-show="notSelectIn">с</span> СВХ</span>
                             </label>
 
                             <label class="label_width_outside_input">
-                                <input type="checkbox" disabled>
+                                <input type="checkbox" v-model="WebInlandTransportation" :disabled="notSelectedMainParams">
                                 <span class="pseudo_checkbox"></span>
-                                <span class="title">заказать автоперевозку до/от терминала</span>
+                                <span class="title">заказать автоперевозку <span v-show="notSelectOut">до</span><span v-show="notSelectIn && notSelectOut">/</span><span v-show="notSelectIn">с</span> терминала</span>
                             </label>
                             <label class="label_width_outside_input">
-                                <input type="checkbox" disabled>
+                                <input type="checkbox" v-model="WebStaffingStripping" :disabled="notSelectedMainParams || notSelectIn">
                                 <span class="pseudo_checkbox"></span>
-                                <span class="title">заказать погрузку/разгрузку груза</span>
+                                <span class="title">заказать <span v-show="bidEmpty !== false">погрузку</span><span v-show="bidEmpty !== true && bidEmpty !== false">/</span><span v-show="bidEmpty !== true">разгрузку</span> контейнера</span>
                             </label>
 
                             <label class="label_width_outside_input">
-                                <input type="checkbox" disabled>
+                                <input type="checkbox" v-model="RepairContainer" :disabled="notSelectedMainParams || notSelectIn">
                                 <span class="pseudo_checkbox"></span>
                                 <span class="title">заказать ремонт контейнера</span>
                             </label>
 
                             <label class="label_width_outside_input">
-                                <input type="checkbox" disabled>
+                                <input type="checkbox" v-model="ReturnContainer" :disabled="notSelectedMainParams || !WebInlandTransportation">
                                 <span class="pseudo_checkbox"></span>
                                 <span class="title">заказать обратную доставку контейнера</span>
                             </label>
 
-
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="application_wrap">
-            <div class="application">
-                <h2>Заявка на прием/выдачу контейнера</h2>
-                <div class="form_row">
-                    <div class="receiving_form_starting">
-
-                        <!-- //todo проставить names в зависимости от того чекбоксы это или радио батоны -->
-                        <div class="labels">
-                            <label class="label_width_outside_input">
-                                <input type="checkbox" v-model="WebGateIn" @click="setMainOperationIn(true)">
-                                <span class="pseudo_checkbox"></span>
-                                <span class="title">Принять на терминал</span>
-                            </label>
-                            <label class="label_width_outside_input">
-                                <input type="checkbox" v-model="WebGateOut" @click="setMainOperationIn(true)">
-                                <span class="pseudo_checkbox"></span>
-                                <span class="title">Выдать с терминала</span>
-                            </label>
-                        </div>
-
-                        <div class="labels" v-if="selectedMainOperation">
-                            <label class="label_width_outside_input">
-                                <input type="checkbox" v-model="DangerousGoods">
-                                <span class="pseudo_checkbox"></span>
-                                <span class="title">Опасный груз</span>
-                            </label>
-                        </div>
-
-                        <div class="labels"  v-if="selectedMainOperation">
-                            <label class="label_width_outside_input">
-                                <input v-model="WebInlandTransportation" type="checkbox">
-                                <span class="pseudo_checkbox"></span>
-                                <span class="title">Заказать автоперевозку</span>
-                            </label>
-                            <label class="label_width_outside_input">
-                                <input v-model="WebStaffingStripping" type="checkbox">
-                                <span class="pseudo_checkbox"></span>
-                                <span class="title">Заказать погрузо-разгрузочные работы</span>
-                            </label>
-                        </div>
-
-                        <div class="labels" v-if="selectedMainOperation">
-                            <label class="label_width_outside_input">
-                                <input v-model="WebCustomsRelease" type="checkbox">
-                                <span class="pseudo_checkbox"></span>
-<!--                                <span v-if="bidType === 'WebGateIn'" class="title">Заказать перемещение из СВХ</span>-->
-<!--                                <span v-if="bidType === 'WebGateOut'" class="title">Переместить в СВХ</span>-->
-                            </label>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <button @click.prevent="createBid" :disabled="!selectedMainOperation" class="cit_btn btn_submit" type="submit">Создать заявку</button>
+        <button @click.prevent="createBid" :disabled="notSelectedMainParams" class="cit_btn btn_submit" type="submit">Создать заявку</button>
     </form>
 </template>
 
@@ -158,19 +102,57 @@
 
         data: function () {
             return {
-                WebGateIn: false,                   // Операция подачи контейнера
-                WebGateOut: false,                  // Операция вывоза контейнера
+                webGate: null,                   // Операция подачи контейнера
+                bidEmpty: null,                   // Операция подачи контейнера
                 DangerousGoods: false,              // Опция опасный груз
                 WebInlandTransportation: false,     // Доставка автотранспортом
                 WebCustomsRelease: false,           // Перемещение из зоны СВХ
                 WebStaffingStripping: false,        // Погрузочно-разгрузочные работы
+                ReturnContainer: false,
+                RepairContainer: false
             }
         },
 
         computed: {
-            selectedMainOperation: function(){
-                return this.WebGateIn || this.WebGateOut;
+
+            notSelectIn: function(){
+              return this.webGate !== "WebGateIn";
             },
+
+            notSelectOut: function(){
+                return this.webGate !== "WebGateOut";
+            },
+
+            notSelectedMainOperation: function(){
+                return this.notSelectIn && this.notSelectOut;
+            },
+
+            notSelectedMainParams: function(){
+                return this.notSelectedMainOperation || (this.bidEmpty !== true && this.bidEmpty !== false);
+            },
+        },
+
+        watch: {
+
+            webGate: function(){
+                this.WebInlandTransportation = false;
+                this.WebCustomsRelease = false;
+                this.WebStaffingStripping = false;
+                this.ReturnContainer = false;
+                this.RepairContainer = false;
+            },
+
+            bidEmpty: function () {
+                if(this.bidEmpty){
+                    this.DangerousGoods = null;
+                }
+            },
+
+            WebInlandTransportation: function () {
+                if(!this.WebInlandTransportation){
+                    this.ReturnContainer = false;
+                }
+            }
         },
 
         methods: {
